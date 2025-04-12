da4af53aa0b947c6cde894ea6fdc5aa68c1aa28f
AdnanMehr8 <adnanhaider4878@gmail.com>
2025-04-12 13:54:51 +0500

    Update API config in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+//   baseURL: "http://localhost:5000",
+  baseURL: "https://ai-dashboard-backend-delta.vercel.app/",
+
 
 //   withCredentials: true,
   headers: {
